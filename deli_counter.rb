76918cc9d401b8463234fab60ceab68432fb4c0a
Toni Lamont <tlam212@gmail.com>
2019-04-19 13:39:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
-
-def line(array) 
-  array.each_with_index do |name, i|
-    if array.size == 0 
+katzDeli = []
+def line(katzDeli) 
+     puts "The line is currently: #{i + 1}. #{name} "
+     if katzDeli.size == 0 
       puts "The line is currently empty."
     else
-     puts "The line is currently: #{i + 1}. #{name} "
     end
   end
   
