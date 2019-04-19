f4f73612bc37f454c4279ad47ce5b5117c37b1ca
Toni Lamont <tlam212@gmail.com>
2019-04-19 13:34:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(array)
   array.each_with_index do |name, i|
     if array.size == 0 
       puts "The line is currently empty."
-   else
+    else
      puts "The line is currently: #{i + 1}. #{name} "
     end
-end
+  end
   
 end
